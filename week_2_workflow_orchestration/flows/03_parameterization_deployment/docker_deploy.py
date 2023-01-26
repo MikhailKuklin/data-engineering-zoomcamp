a97fa1626adb2bafd44dbb1d715566235ebbd9c8
MikhailKuklin <kuklin1990@gmail.com>
2023-01-26 21:04:08 +0000

    daily updates

--- week_2_workflow_orchestration/flows/03_parameterization_deployment/docker_deploy.py
+++ week_2_workflow_orchestration/flows/03_parameterization_deployment/docker_deploy.py
@@ -2,14 +2,13 @@ from prefect.deployments import Deployment
 from parameterized_flow import etl_parent_flow # copy our functions
 from prefect.infrastructure.docker import DockerContainer
 
-docker_block = DockerContainer.load("dezoomcamp")
+docker_block = DockerContainer.load("dezoomcamp2")
 
 docker_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name="docker-flow",
+    name="docker-flow3",
     infrastructure=docker_block,
 )
-
 
 if __name__ == "__main__":
     docker_dep.apply()
