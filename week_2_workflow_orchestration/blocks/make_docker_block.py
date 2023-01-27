8033672a97a3ba19d582ef001cbf15926c8170b9
MikhailKuklin <kuklin1990@gmail.com>
2023-01-27 14:28:56 +0000

    add fixed files

--- week_2_workflow_orchestration/blocks/make_docker_block.py
+++ week_2_workflow_orchestration/blocks/make_docker_block.py
@@ -2,9 +2,9 @@ from prefect.infrastructure.docker import DockerContainer
 
 # alternative to creating DockerContainer block in the UI
 docker_block = DockerContainer(
-    image="mikhailkuklin/prefect_de:dezoomcamp",  # insert your image here
+    image="mikhailkuklin/prefect_de:dezoomcamp3",  # insert your image here
     image_pull_policy="ALWAYS",
     auto_remove=True,
 )
 
-docker_block.save("dezoomcamp2", overwrite=True)
+docker_block.save("dezoomcamp3", overwrite=True)
--- week_2_workflow_orchestration/flows/03_parameterization_deployment/docker_deploy.py
+++ week_2_workflow_orchestration/flows/03_parameterization_deployment/docker_deploy.py
@@ -2,11 +2,11 @@ from prefect.deployments import Deployment
 from parameterized_flow import etl_parent_flow # copy our functions
 from prefect.infrastructure.docker import DockerContainer
 
-docker_block = DockerContainer.load("dezoomcamp2")
+docker_block = DockerContainer.load("dezoomcamp3")
 
 docker_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name="docker-flow3",
+    name="docker-flow6",
     infrastructure=docker_block,
 )
 
