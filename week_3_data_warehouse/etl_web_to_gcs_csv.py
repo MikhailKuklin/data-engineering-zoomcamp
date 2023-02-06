0ff60a051f7ad18d4bdbee70e1713dab52e2b4d6
MikhailKuklin <kuklin1990@gmail.com>
2023-02-06 18:24:16 +0000

    add transferring script for csv

---
+++ week_3_data_warehouse/etl_web_to_gcs_csv.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+
+
+@task()
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read FHV data from web into pandas DataFrame"""
+    #df = pd.read_csv(dataset_url, encoding='latin1')
+    df = pd.read_csv(dataset_url)
+    return df
+
+@task(log_prints=True)
+def clean(df=pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+    df["dropOff_datetime"] = pd.to_datetime(df["dropOff_datetime"])
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+@task()
+def write_local(df: pd.DataFrame, dataset_file: str) -> Path:
+    """Write DataFrame out locally as gz file"""
+    path = Path(f"data/{dataset_file}.csv.gz")
+    df.to_csv(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("prefect-gcs")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+@flow()
+def etl_web_to_gcs(year: int, month: int) -> None:
+    """The main ETL function"""
+    dataset_file = f"fhv_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, dataset_file)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(
+    months: list[int] = [1, 2], year: int = 2021
+):
+    for month in months:
+        etl_web_to_gcs(year, month)
+
+if __name__ == "__main__":
+    months = [1,2,3,4,5,6,7,8,9,10,11,12]
+    year = 2019
+    etl_parent_flow(months, year)
