ae1170f179bd59ba8f2db01f5dd2f92f86f066a3
Mikhail Kuklin <kuklin1990@gmail.com>
2023-02-12 12:11:03 +0000

    homework

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -40,10 +40,8 @@ select
 from tripdata
 where rn = 1
 
+{% if var('is_test_run', default=true) %}
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
----{% if var('is_test_run', default=true) %}
+  limit 100
 
----  limit 100
-
----{% endif %}
+{% endif %}
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -41,9 +41,8 @@ select
 from tripdata
 where rn = 1
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
----{% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=true) %}
 
----limit 100
+limit 100
 
----{% endif %}
+{% endif %}
